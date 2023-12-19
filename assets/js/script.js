156814268a069b78cbda22800eceae5c139bd5cb
Salvatore <mammoliti.sam@gmail.com>
2023-12-19 11:22:48 -0500

    "amended display search script file name"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchInputVal = document.querySelector('#search-input').value;
-    var searchInputVal = document.querySelector('#format-input').value;
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
 
-    if (!searchInputVal) {
-        console.error('You need a search input value');
-        return;
-    }
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
 
-    var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
 
-    location.assign(queryString);
+  location.assign(queryString);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
